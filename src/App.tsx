59daa9fa6d81311f21801ab23e4acd4601c7c6b2
mungase-narayan <narayanmungase03@gmail.com>
2025-03-21 23:33:21 +0530

    add app files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 import router from "./routes/index";
+import store from "./store";
 const App = () => {
   return (
-    <div>
-      <RouterProvider router={router} />
+    <div vaul-drawer-wrapper="">
+      <div className="relative flex min-h-screen flex-col bg-background text-foreground">
+        <Provider store={store}>
+          <RouterProvider router={router} />
+        </Provider>
+      </div>
     </div>
   );
 };
