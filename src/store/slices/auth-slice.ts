22764775d8595f180204f6267c7ebf24bdc9fb50
mungase-narayan <narayanmungase03@gmail.com>
2025-03-21 23:28:01 +0530

    set auth

---
+++ src/store/slices/auth-slice.ts
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { User } from "@/types";
+
+interface AuthSliceType {
+  user: User | null;
+  authToken: string | null;
+  isAuth: boolean;
+}
+
+const initialState: AuthSliceType = {
+  user: null,
+  authToken: null,
+  isAuth: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setAuth: (state, action) => {
+      const { user, authToken } = action.payload;
+      state.user = user;
+      state.authToken = authToken;
+      state.isAuth = true;
+    },
+
+    logout: (state) => {
+      state.user = null;
+      state.authToken = null;
+      state.isAuth = false;
+    },
+
+    setUser: (state, action) => {
+      const user = action.payload;
+      state.user = user;
+    },
+  },
+});
+
+export const { setAuth, logout, setUser } = authSlice.actions;
+export default authSlice.reducer;
