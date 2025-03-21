1ff44f6e23eed296abacad4b36b814f8b733e475
mungase-narayan <narayanmungase03@gmail.com>
2025-03-21 23:27:36 +0530

    add routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,11 @@
 import SignUpPage from "@/features/auth/sign-up";
-import AuthLayout from "@/layouts/auth-layout";
-import RootLayout from "@/layouts/root-layout";
+import DashboardHomePage from "@/features/dashboard/home";
+import HomePage from "@/features/home";
+import PricingPage from "@/features/pricing";
+import AppLayout from "@/layout/app-layout";
+import AuthLayout from "@/layout/auth-layout";
+import DashboardLayout from "@/layout/dashboard-layout";
+import RootLayout from "@/layout/root-layout";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -12,6 +17,15 @@ const router = createBrowserRouter(
     <Route path="/" element={<RootLayout />}>
       <Route path="auth" element={<AuthLayout />}>
         <Route path="sign-up" element={<SignUpPage />}></Route>
+      </Route>
+
+      <Route element={<AppLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="pricing" element={<PricingPage />} />
+      </Route>
+
+      <Route path="dashboard" element={<DashboardLayout />}>
+        <Route index element={<DashboardHomePage />} />
       </Route>
     </Route>
   )
